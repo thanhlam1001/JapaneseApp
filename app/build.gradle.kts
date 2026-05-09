c0a717406cbd4136c4b71d9de16b5f90b2d3462f
thanhlam1001 <baclamtbt1001@gmail.com>
2026-05-09 10:49:37 +0700

    Add Android Material dependency for XML theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
 
     debugImplementation(composeBom)
